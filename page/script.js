4481f189907a898f8a2a2b8999de8878ae4a608d
akaz-cyber <ajrisonic62@gmail.com>
2022-09-30 15:51:21 +0700

    p

--- page/script.js
+++ page/script.js
@@ -2,6 +2,6 @@ function sendMessage(){
     let number = document.getElementById('number').value;
     let name = document.getElementById('name').value;
     let message = document.getElementById('message').value;
-    document.location.href = "https://wa.me/62895347086501?text=Hello%0Aname%20:%20" + name +
-    "%0Aphone%20number%20:%20" + number + "%0Amessage%20:%20" + message, 'target', '_blank';
+     
+    window.open('https://wa.me/62895347086501?text=Hello%0Aname%20:%20' + name + '%0Aphone%20number%20:%20' + number + '%0Amessage%20:%20' + message, '_blank');
 }
